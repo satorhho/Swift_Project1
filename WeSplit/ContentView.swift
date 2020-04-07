29a4feaaa52a1c937af19df5e108c2f45533ec04
satorhho <lalancebriones314@gmail.com>
2020-04-07 19:59:23 +0800

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Lance Kent Briones on 4/7/20.
+//  Copyright © 2020 Lance Kent Briones. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
