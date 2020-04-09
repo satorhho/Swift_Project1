093e1cbe61dce36a14e83057ea0a2b45ae45ae0a
satorhho <lalancebriones314@gmail.com>
2020-04-09 13:47:13 +0800

    Final

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,8 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var check_amount: String = ""
+    @State private var number_of_people: Int = 0
+    @State private var tip_percentage: Int = 2
+    
+    var shared_amount: Double {
+        let check_store = Double(check_amount) ?? 0
+        let percentage_store = Double(percentages[tip_percentage])
+        let people_store = Double(number_of_people + 1)
+        
+        let percentage = check_store * (percentage_store * pow(Double(10), Double(-2)))
+        let grand = check_store + percentage
+        let output = grand / people_store
+        
+        return output
+    }
+    
+    let percentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            Form {
+                Section {
+                    TextField("Amount:", text: $check_amount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people:", selection: $number_of_people){
+                        ForEach(1..<100){
+                            Text("\($0)")
+                        }
+                    }
+                }
+                Section(header: Text("Tip Percentage:")){
+                    Picker("%", selection: $tip_percentage){
+                        ForEach(0..<percentages.count){
+                            Text("\(self.percentages[$0])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section{
+                    Text("$\(self.shared_amount, specifier: "%.2f")")
+                }
+            }.navigationBarTitle(Text("WeSplit"), displayMode: .large)
+        }
     }
 }
 
